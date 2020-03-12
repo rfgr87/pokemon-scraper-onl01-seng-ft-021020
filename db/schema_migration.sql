452668cbd36f2ba8d743fc5da036d128c1127b39
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 16:45:22 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,10 +1,5 @@
 -- Write your sql commands to create your db schema here.
 
-sqlite3 pokemon.db
-
-touch db/pokemon.db
-
-sqlite3 path/to/pokemon.db < schema_migration.sql
 
 -- It should create a table in a new table in the db/ directory.
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
