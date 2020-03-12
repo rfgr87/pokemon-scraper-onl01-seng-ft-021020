ad38034025d21a82604a8aa2fbb6651bc2d9320a
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 17:07:20 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@
 DB = {:conn => SQLite3::Database.new("db/pokemon.db")}
 
 class Pokemon
-  attr_accessor :name, :type
+  attr_accessor :name, :type, :db
   attr_reader :id
   
-  def initialize(id=nil, name, type)
+  def initialize(id=nil, name, type, db)
     @id = id
     @name = name 
     @type = type
