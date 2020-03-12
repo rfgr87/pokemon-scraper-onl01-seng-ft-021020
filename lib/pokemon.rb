b78aa09b9f0a971c5ca69f8b4ed1e548bfbebe88
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-12 17:03:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,5 +2,15 @@
 DB = {:conn => SQLite3::Database.new("db/pokemon.db")}
 
 class Pokemon
-
+  attr_accessor :name, :type
+  attr_reader :id
+  
+  def initialize(id=nil, name, type)
+    @id = id
+    @name = name 
+    @type = type
+  end
+  
+  def self.save
+    
 end
